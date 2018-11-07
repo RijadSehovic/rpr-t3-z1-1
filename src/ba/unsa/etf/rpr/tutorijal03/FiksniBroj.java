254a1298de5707f1efc877db22ab04c98a347401
Emerald <epodbicani1@etf.unsa.ba>
2018-11-07 20:26:07 +0100

    imenik

--- src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
@@ -1,6 +1,9 @@
 package ba.unsa.etf.rpr.tutorijal03;
+
 import java.util.Objects;
-public class FiksniBroj extends TelefonskiBroj {
+
+public class FiksniBroj extends TelefonskiBroj implements Comparable<TelefonskiBroj> {
+
     public enum Grad{
         SARAJEVO("033"),
         TUZLA("035"),
@@ -22,9 +25,15 @@ public class FiksniBroj extends TelefonskiBroj {
         PALE("057"),
         FOCA("058"),
         TREBINJE("059");
+
         private final String pozivniBroj;
+
         Grad(String pozivniBroj){
             this.pozivniBroj=pozivniBroj;
+        }
+
+        public final String getPozivniBroj(){
+            return pozivniBroj;
         }
     }
     private Grad grad;
@@ -33,22 +42,38 @@ public class FiksniBroj extends TelefonskiBroj {
         this.grad=grad;
         this.broj=broj;
     }
-    /*public String ispisi(){
-   @Override
-   public boolean equals(Object o) {
-       if (this == o) return true;
-       if (!(o instanceof FiksniBroj)) return false;
-       FiksniBroj that = (FiksniBroj) o;
-       return grad == that.grad &&
-               Objects.equals(broj, that.broj);
-   }
-    }*/
+
+    public Grad getGrad() {
+        return grad;
+    }
+
+    public void setGrad(Grad grad) {
+        this.grad = grad;
+    }
+
+    @Override
+    public int compareTo(TelefonskiBroj broj) {
+        return this.ispisi().compareTo(broj.ispisi());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof FiksniBroj)) return false;
+        FiksniBroj that = (FiksniBroj) o;
+        return grad == that.grad &&
+                Objects.equals(broj, that.broj);
+    }
+
     @Override
     public int hashCode() {
         return Objects.hash(grad, broj);
     }
+
     @Override
     public String ispisi(){
-        return grad+"/"+broj;
+        return grad.pozivniBroj+"/"+broj;
     }
-}
+
+
+}}
--- src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
+++ src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
@@ -1,15 +1,73 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
+import java.util.*;
+
+
 public class Imenik {
 
+    private HashMap<String, TelefonskiBroj> mapa;
 
-    public void dodaj(String ime, TelefonskiBroj broj){}
+    Imenik(){
+        mapa= new HashMap<String, TelefonskiBroj>();
+    }
 
-    String dajBroj(String ime){ return null; }
+    void dodaj(String ime, TelefonskiBroj broj){
+        mapa.put(ime,broj);
+    }
 
-    String dajIme(TelefonskiBroj broj) { return null; }
+    String dajBroj(String ime){
+        return mapa.get(ime).ispisi();
+    }
 
-    String naSlovo(char s) { return null;  }
+    String dajIme(TelefonskiBroj broj){
+        Iterator it=mapa.entrySet().iterator();
+        while(it.hasNext()){
+            HashMap.Entry<String,TelefonskiBroj> par= (HashMap.Entry<String, TelefonskiBroj>) it.next();
+            if(par.getValue()==broj) return par.getKey();
+        }
+        return "";
+    }
+
+    String naSlovo(char s) {
+        String ispis="";
+        int brojac=1;
+        Iterator it = mapa.entrySet().iterator();
+        while (it.hasNext()) {
+            HashMap.Entry<String, TelefonskiBroj> par = (HashMap.Entry<String, TelefonskiBroj>) it.next();
+            if(par.getKey().charAt(0)==s) {
+                ispis = ispis + brojac + ". " + par.getKey() + " - " + par.getValue().ispisi() + "\n";
+                brojac++;
+            }
+        }
+        return ispis;
+    }
+
+    Set<String> izGrada(FiksniBroj.Grad g){
+        TreeSet<String> skup=new TreeSet<String>();
+        Iterator it=mapa.entrySet().iterator();
+        while(it.hasNext()){
+            HashMap.Entry<String, TelefonskiBroj> par = (HashMap.Entry<String, TelefonskiBroj>) it.next();
+            if (par.getValue() instanceof FiksniBroj) {
+                if ( ( (FiksniBroj) par.getValue() ).getGrad().equals(g) )
+                    skup.add(par.getKey());
+            }
+        }
+        return skup;
+    }
+
+    Set<TelefonskiBroj> izGradaBrojevi(FiksniBroj.Grad g){
+        TreeSet<TelefonskiBroj> brojeviIzGrada = new TreeSet<>(); //TreeSet u koji smjestamo odgovarajuce TelefonskeBrojeve sortirana po Stringu ispisi();
+        Iterator it = mapa.entrySet().iterator();
+        while (it.hasNext()) {
+            HashMap.Entry<String, TelefonskiBroj> par = (HashMap.Entry<String, TelefonskiBroj>) it.next();
+            if (par.getValue() instanceof FiksniBroj) { //ako je odg. Object instanca klase FiksniBroj i ako je grad tog broja jednak parametru g onda dodajemo u skup odg string koji je ime i prezime ;
+                if (g.equals(((FiksniBroj) par.getValue()).getGrad())) {
+                    brojeviIzGrada.add(par.getValue());
+                }
+            }
+        }
+        return brojeviIzGrada;
+    }
 
 
 }
