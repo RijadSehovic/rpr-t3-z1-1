36db6a3ba3d5de561a894ec9a2f2711ee403a77d
Emerald <epodbicani1@etf.unsa.ba>
2018-11-07 20:23:09 +0100

    medjunarodni broj

--- src/ba/unsa/etf/rpr/tutorijal03/MedunarodniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/MedunarodniBroj.java
@@ -1,20 +1,26 @@
 package ba.unsa.etf.rpr.tutorijal03;
-
-public class MedunarodniBroj extends TelefonskiBroj {
-    @Override
-    public String ispisi() {
-        return null;
+import java.util.Objects;
+public class MedunarodniBroj {
+    private String drzava;
+    private String broj;
+    MedunarodniBroj(String drzava, String broj){
+        this.drzava=drzava;
+        this.broj=broj;
     }
-
+    @Override
+    public String ispisi(){
+        return drzava+"/"+broj;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof MedunarodniBroj)) return false;
+        MedunarodniBroj that = (MedunarodniBroj) o;
+        return Objects.equals(drzava, that.drzava) &&
+                Objects.equals(broj, that.broj);
+    }
     @Override
     public int hashCode() {
-        return 0;
-    }
-    String drzava;
-    String broj;
-
-    public MedunarodniBroj(String drz, String br){
-        drzava = drz;
-        broj = br;
+        return Objects.hash(drzava, broj);
     }
 }
